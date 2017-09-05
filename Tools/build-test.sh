f9c66887de9bbcfcf50ccabcc27ec5f74ba52bd6
Gleb Balykov <g.balykov@yandex.ru>
2017-09-05 09:57:20 +0300

    Add build test for fdtd3d

---
+++ Tools/build-test.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# Home directory of project where root CMakeLists.txt is placed
+HOME_DIR=$1
+
+# Build directory of unit test
+BUILD_DIR=$2
+
+# CXX compiler
+CXX_COMPILER=$3
+
+# C compiler
+C_COMPILER=$4
+
+BUILD_MODE=$5
+
+cd ${BUILD_DIR}
+
+function build
+{
+  PARALLEL_GRID_MODE=$1
+  PARALLEL_GRID_DIM=$2
+  LIST_OF_BUFFERS="$3"
+
+  for VALUE_TYPE in f d ld; do
+    for TIME_STEPS in 1 2; do
+      for COMPLEX_FIELD_VALUES in ON OFF; do
+        for CXX11_ENABLED in ON OFF; do
+          for PRINT_MESSAGE in ON OFF; do
+            for PARALLEL_BUFFER in `echo $LIST_OF_BUFFERS`; do
+              if [ "${VALUE_TYPE}" == "ld" ] && [ "${COMPLEX_FIELD_VALUES}" == "ON" ]; then
+                continue
+              fi
+
+              cmake ${HOME_DIR} -DCMAKE_BUILD_TYPE=${BUILD_MODE} \
+                -DVALUE_TYPE=${VALUE_TYPE} \
+                -DCOMPLEX_FIELD_VALUES=${COMPLEX_FIELD_VALUES} \
+                -DTIME_STEPS=${TIME_STEPS} \
+                -DPARALLEL_GRID_DIMENSION=${PARALLEL_GRID_DIM} \
+                -DPRINT_MESSAGE=${PRINT_MESSAGE} \
+                -DPARALLEL_GRID=${PARALLEL_GRID_MODE} \
+                -DPARALLEL_BUFFER_DIMENSION=${PARALLEL_BUFFER} \
+                -DCXX11_ENABLED=${CXX11_ENABLED} \
+                -DCUDA_ENABLED=OFF \
+                -DCUDA_ARCH_SM_TYPE=sm_50 \
+                -DCMAKE_CXX_COMPILER=${CXX_COMPILER} \
+                -DCMAKE_C_COMPILER=${C_COMPILER}
+
+              res=$(echo $?)
+
+              if [[ res -ne 0 ]]; then
+                exit 1
+              fi
+
+              make fdtd3d
+
+              res=$(echo $?)
+
+              if [[ res -ne 0 ]]; then
+                exit 1
+              fi
+            done
+          done
+        done
+      done
+    done
+  done
+}
+
+#array1D="x"
+#build ON 1 "$array1D"
+#build OFF 1 "x"
+
+#array2D="x y xy"
+#build ON 2 "$array2D"
+#build OFF 2 "x"
+
+array3D="x y z xy yz xz xyz"
+build ON 3 "$array3D"
+build OFF 3 "x"
+
+exit 0
