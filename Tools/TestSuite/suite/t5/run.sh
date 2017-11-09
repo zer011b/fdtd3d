fb1f7ac82c1da363d89f01decf7b83782110fe30
Gleb Balykov <g.balykov@yandex.ru>
2017-11-09 11:56:22 +0300

    Improve test

--- Tools/TestSuite/suite/t5/run.sh
+++ Tools/TestSuite/suite/t5/run.sh
@@ -38,41 +38,61 @@ dx2="0.0005"
 size3="80"
 dx3="0.00025"
 
-launch $size1 101 $dx1
-diff1=$(echo $max_diff)
-if [ $? -ne 0 ]; then
-  retval=$((1))
-fi
+function test ()
+{
+  timesteps=$1
+  local ret=$((0))
 
-launch $size2 201 $dx2
-diff2=$(echo $max_diff)
-if [ $? -ne 0 ]; then
-  retval=$((1))
-fi
+  timesteps1=$(echo $timesteps | awk '{print $1 + 1}')
+  launch $size1 $timesteps1 $dx1
+  diff1=$(echo $max_diff)
+  if [ $? -ne 0 ]; then
+    ret=$((1))
+  fi
 
-launch $size3 401 $dx3
-diff3=$(echo $max_diff)
-if [ $? -ne 0 ]; then
-  retval=$((1))
-fi
+  timesteps2=$(echo $timesteps | awk '{print $1 * 2 + 1}')
+  launch $size2 $timesteps2 $dx2
+  diff2=$(echo $max_diff)
+  if [ $? -ne 0 ]; then
+    ret=$((1))
+  fi
 
-is_ok=$(echo $diff1 $diff2 $diff3 | awk '
-          {
-            d1 = $1 / $2;
-            d2 = $2 / $3;
-            if (4.0 < d1 && d1 < 4.1 && 4.0 < d2 && d2 < 4.1)
+  timesteps3=$(echo $timesteps | awk '{print $1 * 4 + 1}')
+  launch $size3 $timesteps3 $dx3
+  diff3=$(echo $max_diff)
+  if [ $? -ne 0 ]; then
+    ret=$((1))
+  fi
+
+  is_ok=$(echo $diff1 $diff2 $diff3 | awk '
             {
-              print 1;
-            }
-            else
-            {
-              print 0;
-            }
-          }')
-if [ "$is_ok" != "1" ]; then
-  echo "Second order of accuracy failed"
-  retval=$((2))
-  break
+              d1 = $1 / $2;
+              d2 = $2 / $3;
+              if (4.0 < d1 && 4.0 < d2)
+              {
+                print 1;
+              }
+              else
+              {
+                print 0;
+              }
+            }')
+  if [ "$is_ok" != "1" ]; then
+    echo "Second order of accuracy failed: $diff1 $diff2 $diff3"
+    ret=$((2))
+  fi
+
+  return $ret
+}
+
+test 100
+if [ $? -ne 0 ]; then
+  retval=$((1))
+fi
+
+test 200
+if [ $? -ne 0 ]; then
+  retval=$((1))
 fi
 
 cd $CUR_DIR
