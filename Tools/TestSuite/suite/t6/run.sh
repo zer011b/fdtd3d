57c86b1b63301b93c3b50276553a0b9ee0076c3d
Gleb Balykov <g.balykov@yandex.ru>
2018-06-17 22:23:56 +0300

    Add Examples/vacuum3D.txt to test suite

---
+++ Tools/TestSuite/suite/t6/run.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+BASE_DIR=$1
+SOURCE_DIR=$2
+
+function launch ()
+{
+  ./fdtd3d --cmd-from-file ${SOURCE_DIR}/Examples/vacuum3D.txt &> /tmp/fdtd3d.txt
+
+  local ret=$?
+
+  val_max=$(cat current[100]_rank-0_Ey-Mod.bmp.txt | awk '{print $1}')
+  val_min=$(cat current[100]_rank-0_Ey-Mod.bmp.txt | awk '{print $2}')
+  is_ok=$(echo $val_max $val_min | awk '
+            {
+              if (1.01 <= $1 && $1 <= 1.02 && 0.0 == $2)
+              {
+                print 1;
+              }
+              else
+              {
+                print 0;
+              }
+            }')
+  if [ "$is_ok" != "1" ]; then
+    echo "Test result is incorrect for Examples/vacuum3D.txt"
+    ret=$((2))
+  fi
+
+  if [ "$ret" != "0" ]; then
+    return $ret
+  fi
+
+  ./fdtd3d --cmd-from-file ${SOURCE_DIR}/Examples/vacuum3D_scattered.txt &> /tmp/fdtd3d.txt
+
+  ret=$?
+
+  val_max=$(cat current[100]_rank-0_Ey-Mod.bmp.txt | awk '{print $1}')
+  val_min=$(cat current[100]_rank-0_Ey-Mod.bmp.txt | awk '{print $2}')
+  is_ok=$(echo $val_max $val_min | awk '
+            {
+              if (0.0 == $1 && 0.0 == $2)
+              {
+                print 1;
+              }
+              else
+              {
+                print 0;
+              }
+            }')
+  if [ "$is_ok" != "1" ]; then
+    echo "Test result is incorrect for Examples/vacuum3D_scattered.txt"
+    ret=$((2))
+  fi
+
+  return $ret
+}
+
+CUR_DIR=`pwd`
+TEST_DIR=$(dirname $(readlink -f $0))
+cd $TEST_DIR
+
+retval=$((0))
+launch
+if [ $? -ne 0 ]; then
+  retval=$((1))
+fi
+
+cd $CUR_DIR
+
+exit $retval
