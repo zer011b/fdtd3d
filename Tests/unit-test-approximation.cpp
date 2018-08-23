aa93b98181bb5d470cc3f81f62929d85f41318db
Gleb Balykov <g.balykov@yandex.ru>
2018-08-23 23:53:30 +0300

    Add unit test for approximation

---
+++ Tests/unit-test-approximation.cpp
@@ -0,0 +1,43 @@
+/*
+ * Unit test for basic operations with Clock
+ */
+
+#include <iostream>
+
+#include "Approximation.h"
+#include "Clock.h"
+
+int main (int argc, char** argv)
+{
+#ifndef DEBUG_INFO
+  ALWAYS_ASSERT_MESSAGE ("Test requires debug info");
+#endif /* !DEBUG_INFO */
+
+  FPValue lambda = 0.04;
+
+  FPValue stepAngle = PhysicsConst::Pi / 12;
+  for (FPValue angle1 = FPValue (0); angle1 < 2 * PhysicsConst::Pi + stepAngle; angle1 += stepAngle)
+  {
+    for (FPValue angle2 = FPValue (0); angle2 < 2 * PhysicsConst::Pi + stepAngle; angle2 += stepAngle)
+    {
+      for (FPValue courantNum = FPValue (0.5); courantNum <= FPValue (1.0); courantNum += FPValue (0.25))
+      {
+        for (FPValue delta = FPValue (0.0002); delta <= FPValue (0.002); delta *= 2)
+        {
+          FPValue N_lambda = lambda / delta;
+          ALWAYS_ASSERT (((FPValue)((int)N_lambda)) == N_lambda);
+
+          FPValue general_k = Approximation::approximateWaveNumberGeneral (delta, lambda, courantNum,
+                                                                           N_lambda, angle1, angle2);
+          FPValue k = Approximation::approximateWaveNumber (delta, lambda, courantNum, N_lambda, angle1, angle2);
+
+          // Accuracy is 1%
+          FPValue accuracy = 2 * PhysicsConst::Pi / lambda * (1 / 100.0);
+          ALWAYS_ASSERT (SQR (k - general_k) < accuracy);
+        }
+      }
+    }
+  }
+
+  return 0;
+} /* main */
---
+++ Tools/Tests/build-and-run-unit-test-approximation.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# Home directory of project where root CMakeLists.txt is placed
+HOME_DIR=$1
+
+# Build directory of unit test
+BUILD_DIR=$2
+
+# CXX compiler
+CXX_COMPILER=$3
+
+# C compiler
+C_COMPILER=$4
+
+CXX11_ENABLED=$5
+
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+function build
+{
+  for VALUE_TYPE in f d ld; do
+    for TIME_STEPS in 1 2; do
+      for COMPLEX_FIELD_VALUES in ON OFF; do
+      for LARGE_COORDINATES in ON OFF; do
+
+        if [ "${VALUE_TYPE}" == "ld" ] && [ "${COMPLEX_FIELD_VALUES}" == "ON" ]; then
+          continue
+        fi
+
+        cmake ${HOME_DIR} -DCMAKE_BUILD_TYPE=Debug \
+          -DVALUE_TYPE=${VALUE_TYPE} \
+          -DCOMPLEX_FIELD_VALUES=${COMPLEX_FIELD_VALUES} \
+          -DTIME_STEPS=${TIME_STEPS} \
+          -DPARALLEL_GRID_DIMENSION=3 \
+          -DPRINT_MESSAGE=ON \
+          -DPARALLEL_GRID=OFF \
+          -DPARALLEL_BUFFER_DIMENSION=x \
+          -DCXX11_ENABLED=${CXX11_ENABLED} \
+          -DCUDA_ENABLED=OFF \
+          -DCUDA_ARCH_SM_TYPE=sm_50 \
+          -DLARGE_COORDINATES=${LARGE_COORDINATES} \
+          -DCMAKE_CXX_COMPILER=${CXX_COMPILER} \
+          -DCMAKE_C_COMPILER=${C_COMPILER} \
+          -DDYNAMIC_GRID=OFF \
+          -DCOMBINED_SENDRECV=OFF \
+          -DMPI_CLOCK=OFF
+
+        res=$(echo $?)
+
+        if [[ res -ne 0 ]]; then
+          exit 1
+        fi
+
+        make unit-test-approximation
+
+        res=$(echo $?)
+
+        if [[ res -ne 0 ]]; then
+          exit 1
+        fi
+
+        ./Tests/unit-test-approximation
+
+        res=$(echo $?)
+
+        if [[ res -ne 0 ]]; then
+          exit 1
+        fi
+      done
+      done
+    done
+  done
+}
+
+build
+
+exit 0
