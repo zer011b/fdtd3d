6545aea76eafce2478c355545021d0d8eeb73b2e
Gleb Balykov <g.balykov@yandex.ru>
2018-10-06 22:47:52 +0300

    Fix cuda package install

--- Tools/travis-cuda-helper.sh
+++ Tools/travis-cuda-helper.sh
@@ -2,6 +2,6 @@
 
 CUDA_REPO_PKG=cuda-repo-ubuntu1404_10.0.130-1_amd64.deb
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/${CUDA_REPO_PKG}
-dpkg -i ${CUDA_REPO_PKG}
+sudo dpkg -i ${CUDA_REPO_PKG}
 rm ${CUDA_REPO_PKG}
 sudo apt-get install cuda
