8f4a4d905a36c1b8d3c7a3c2510826d19e8aace6
Gleb Balykov <g.balykov@yandex.ru>
2019-10-20 22:38:40 +0200

    Update travis env to xenial

--- Tools/travis-helper.sh
+++ Tools/travis-helper.sh
@@ -2,9 +2,6 @@
 
 set -ex
 
-sudo curl -sSL https://cmake.org/files/v3.12/cmake-3.12.3-Linux-x86_64.tar.gz | sudo tar -xzC /opt
-export PATH=/opt/cmake-3.12.3-Linux-x86_64/bin:$PATH
-export LD_LIBRARY_PATH=/opt/cmake-3.12.3-Linux-x86_64/lib:$LD_LIBRARY_PATH
 export HOME_DIR=`pwd`
 export BUILD_DIR="${HOME_DIR}/Build"
 mkdir ${BUILD_DIR}
