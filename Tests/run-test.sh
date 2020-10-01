6d6ede4a9aee44a87de81a2137867d413e19d472
Gleb Balykov <g.balykov@yandex.ru>
2020-10-02 00:22:02 +0300

    Move Tools/TestSuite -> Tests

---
+++ Tests/run-test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -ex
+
+TEST=$1
+shift
+
+ARGS=$@
+
+CUR_DIR=`pwd`
+
+# check exit code
+function check_res ()
+{
+  if [ $? -ne 0 ]; then
+    exit 1
+  fi
+}
+
+echo "TESTING SEQUENTIAL"
+$CUR_DIR/Tests/suite/$TEST/build.sh $ARGS 0
+check_res
+$CUR_DIR/Tests/suite/$TEST/run.sh $ARGS 0
+check_res
+$CUR_DIR/Tests/suite/$TEST/cleanup.sh $ARGS
+check_res
+
+echo "TESTING PARALLEL"
+$CUR_DIR/Tests/suite/$TEST/build.sh $ARGS 2
+check_res
+$CUR_DIR/Tests/suite/$TEST/run.sh $ARGS 2
+check_res
+$CUR_DIR/Tests/suite/$TEST/cleanup.sh $ARGS
+check_res
